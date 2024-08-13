015a98e18744d48e395423ba36eaf7e551f52982
JosefinaFinkel <j.finkel@alumno.um.edu.ar>
2024-08-13 13:34:00 -0300

    Configurando el repositorio

---
+++ main.py
@@ -0,0 +1,12 @@
+import unittest
+
+def sumar(a,b):
+    return a+b
+
+if __name__ == '__main__':
+    sumar(30,30)
+
+
+class Test(unittest.TestCase):
+    def test_sumar(self):
+        self.assertEqual(sumar(30,30),60)
