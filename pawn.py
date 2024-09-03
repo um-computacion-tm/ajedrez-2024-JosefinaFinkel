411e8402c0bf96c336c292d9c3486a326b23ac2c
JosefinaFinkel <j.finkel@alumno.um.edu.ar>
2024-09-03 20:11:22 -0300

    pawn

--- pawn.py
+++ pawn.py
@@ -2,3 +2,15 @@ from piece import Piece
 class Pawn(Piece):
     white_str_ = "♖"
     black_str = "♜"
+
+def possible_positions_vd(self, row, col):
+        possibles = []
+        for next_row in range(row + 1, 8):
+            possibles.append((next_row, col))
+        return possibles
+    
+def possible_positions_va(self, row, col):
+        possibles = []
+        for next_row in range(row - 1, -1, -1):
+            possibles.append((next_row, col))
+        return possibles
