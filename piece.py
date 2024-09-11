d4777c270cbb3917bfd149518188a507706756c5
JosefinaFinkel <j.finkel@alumno.um.edu.ar>
2024-09-11 18:51:07 -0300

    movimientos

--- piece.py
+++ piece.py
@@ -10,6 +10,10 @@ def __str__(self):
        return self.white_str()
    else:
        return self.black_str()
+   
+def can_move(self, start_pos, end_pos, board):
+    # verificar si la pieza puede moverse de start_pos a end_pos
+    pass
 
    
 class Rook(Piece):
