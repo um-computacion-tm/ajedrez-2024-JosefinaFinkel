b953d069dcdf87b71d81a84bc215429fc01ad00b
JosefinaFinkel <j.finkel@alumno.um.edu.ar>
2024-10-21 18:53:53 -0300

    cambios para code climate

--- king.py
+++ king.py
@@ -17,19 +17,39 @@
 #         # Devuelve el símbolo del rey según su color.
 #         return "♔" if self.get_color() == "WHITE" else "♚"
 
+# AMBOS FUNCIONAN, NO BORRAR A MENOS DE ESTAR SEGURA
+
 from piece import Piece
 from movimientos import ReglasDeMovimientos
 
-class King(Piece):
+# class King(Piece):
     
-    def __init__(self, color):
-        super().__init__(color)
-        self.movimientos = ReglasDeMovimientos()  # Usar el nuevo enfoque para movimientos
+#     def __init__(self, color):
+#         super().__init__(color)
+#         self.movimientos = ReglasDeMovimientos()  # Usar el nuevo enfoque para movimientos
 
-    def valid_moves(self, from_row, from_col, to_row, to_col, board):
-        # Valida el movimiento del rey
-        self.movimientos.king_movement(from_row, from_col, to_row, to_col)
-        return True
+#     def valid_moves(self, from_row, from_col, to_row, to_col, board):
+#         # Valida el movimiento del rey
+#         self.movimientos.king_movement(from_row, from_col, to_row, to_col)
+#         return True
 
-    def __str__(self):
-        return "♔" if self.get_color() == "WHITE" else "♚"
+#     def __str__(self):
+#         return "♔" if self.get_color() == "WHITE" else "♚"
+
+class King(Piece):
+        
+        def __init__(self, color):
+            super().__init__(color)
+            self._initialize_movements()
+
+        def _initialize_movements(self):
+            """Inicializa las reglas de movimientos para el rey"""
+            self.movimientos = ReglasDeMovimientos()  # Mover la lógica común aquí
+
+        def valid_moves(self, from_row, from_col, to_row, to_col, board):
+            self.movimientos.king_movement(from_row, from_col, to_row, to_col)
+            return True
+
+        def __str__(self):
+            return "♔" if self.get_color() == "WHITE" else "♚"
+
